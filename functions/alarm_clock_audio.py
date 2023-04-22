b53ed00571b54cfe912d67fff895ab62071ca06d
caden-a-scott <131490895+caden-a-scott@users.noreply.github.com>
2023-04-22 11:04:32 -0400

    Add files via upload

---
+++ functions/alarm_clock_audio.py
@@ -0,0 +1,27 @@
+
+# Imports
+from pygame import mixer
+from time import sleep
+import random, os
+
+# Song Class
+class Song:
+    def __init__(self):
+        pass
+
+    def randomizeSong(self):
+        path = os.getcwd()
+        path = os.path.dirname(path)
+        song_files_path = path + "\\" + "Song_WAV_Files"
+        random_song_path = song_files_path + "\\" + random.choice(os.listdir(song_files_path))
+        self.path = random_song_path
+
+    # Starts playing a random song from the path asynchronously
+    def startRandomSong(self):
+        self.randomizeSong()
+        mixer.init()
+        mixer.music.load(self.path)
+        mixer.music.set_volume(0.7)
+        mixer.music.play()
+
+
---
+++ functions/alarm_clock_light_functions.py
@@ -0,0 +1,35 @@
+
+# Imports
+from time import sleep
+import neopixel
+import board
+from rpi_ws281x import *
+import random
+from operator import add
+import datetime
+
+# Constants
+LED_COUNT = 10
+B = 20
+
+# Colors
+OFF = [0,0,0]
+RED = [B*255/255,0,0]
+YELLOW = [B*255/255,B*255/255,0]
+GREEN = [0,B*255/255,0]
+CYAN = [0,B*255/255,B*255/255]
+BLUE = [0,0,B*255/255]
+PURPLE = [B*255/255,0,B*255/255]
+WHITE = [B*255/255,B*255/255,B*255/255]
+
+
+pixels = neopixel.Neopixel(board.D18,LED_COUNT)
+
+
+
+# Main
+pixels.fill(OFF)
+pixels.fill(CYAN)
+sleep(10)
+pixels.fill(OFF)
+
